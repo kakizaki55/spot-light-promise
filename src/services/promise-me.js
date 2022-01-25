7debe03ccacb6bfbf54a17c050e5d9650e1dbfa1
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-25 11:53:06 -0800

    done with sopt light fetching

--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,35 +1,38 @@
 /**
- * TODO:
- */
+ * TODO: write a fetch request using .then()
+ * TODO: write a fetch request using async await 
+//  */
 
-/**
- * TODO:
- */
+export const getAnimeList = async () => {
+  const respone = await fetch(
+    'https://api.jikan.moe/v4/anime?q=dragon ball&sfw'
+  );
+  const animeData = await respone.json();
+  console.log('inside the fetch', animeData.data);
+};
 
-/**
- * TODO:
- */
+export const getAnimeListThen = () => {
+  return fetch('https://api.jikan.moe/v4/anime?q=dragon ball&sfw').then(
+    (data) =>
+      data.json().then((animeData) => console.log('then data', animeData.data))
+  );
+};
 
-/**
- * TODO:
- */
+export const getPinballData = async () => {
+  const respone = await fetch(
+    'https://ponball.herokuapp.com/api/v1/machines?limit=20'
+  );
+  const temp = await respone.json();
 
-/**
- * TODO:
- */
+  const newData = temp.map((item) => {
+    return { Name: item.title, date: item.date };
+  });
+  console.log('pinball', newData);
+};
 
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
+export const getPinballDataThen = () => {
+  return fetch('https://ponball.herokuapp.com/api/v1/machines?limit=20').then(
+    (data) =>
+      data.json().then((response) => console.log('pin ball then', response))
+  );
+};
